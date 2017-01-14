a4ac03b1804c2600b82fdd6971aa30ff1affd797
Hendrik Mans <hendrik@mans.de>
2017-01-14 15:06:11 +0100

    Create new movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,24 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to @movie
+    else
+      render action: "new"
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :year, :description)
+  end
 end
