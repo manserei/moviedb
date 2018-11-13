a27bb468bbbb8473ba221683364fe1df5de6257f
Hendrik Mans <hendrik@mans.de>
2018-11-13 16:15:53 +0100

    Add some simple ActorController specs.

--- spec/controllers/actors_controller_spec.rb
+++ spec/controllers/actors_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe ActorsController, type: :controller do
+  let!(:actor) { create(:actor) }
 
+  describe "GET index" do
+    it "assigns @actors" do
+      get :index
+      expect(assigns(:actors)).to eq([actor])
+    end
+  end
+
+  describe "GET show" do
+    it "assigns @actor" do
+      get :show, params: { id: actor.id }
+      expect(assigns(:actor)).to eq(actor)
+    end
+  end
 end
